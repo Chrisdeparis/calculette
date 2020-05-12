629608405a8c853582319f1dbb7be90d7c03fc9d
Chrisdeparis <milfortchristian@gmail.com>
2020-05-12 22:17:05 +0200

    3 files modified, 1 added

--- components/Button.js
+++ components/Button.js
@@ -1,27 +1,13 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 
 class Button extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      current: 0,
-      previous: []
+    render(){
+        return (
+            <div className={`column-${this.props.cols}`}>
+                <button className="calc-button" onClick={() => this.props.action(this.props.symbol)}>{this.props.symbol}</button>
+            </div>
+        )
     }
-  }
-  
-  render(){
-    return (
-      <div>
-        <button 
-          class="calc-button" 
-          onClick={() => this.props.action(this.props.symbol)}
-          type="text" 
-          value={this.state.current} />
-        
-      </div>
-    )
-  }
 }
 
 export default Button;
